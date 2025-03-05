7d233adb390bbc8dab9a2cb4dd5386ebaf16ad08
manuela <manuelajuan26@gmail.com>
2025-03-05 14:14:15 -0500

    public video

--- src/DataGlobal/DataServices.jsx
+++ src/DataGlobal/DataServices.jsx
@@ -7,7 +7,7 @@ export const DataServices = [
   {
     id: 1,
     urlImage: "/video/lavadodealfombras.mp4",
-    name: "Limpieza Profunda de Muebles, Alfombras y Vidrios",
+    name: "Limpieza profunda de muebles, alfombras y vidrios",
     icon: <MdOutlineCleanHands className="icon-clean" />,
     description:
       "¡Dile adiós al polvo y la mugre! Con nuestra limpieza profunda, tus muebles, alfombras y vidrios lucirán como nuevos. Utilizamos productos y equipos de última generación para garantizar resultados impecables.",
@@ -25,7 +25,7 @@ export const DataServices = [
   {
     id: 3,
     urlImage: "/video/PisosMantenimiento.mp4",
-    name: "Mantenimiento y Restauracion de Pisos",
+    name: "Mantenimiento y restauración de pisos",
     icon: <BsWrenchAdjustableCircle className="icon-clean" />,
     description:
       "¿Tus pisos están dañados o desgastados? Nuestros profesionales restaurarán su apariencia, dejándolos como nuevos. Utilizamos técnicas y productos especializados para cada tipo de piso.",
@@ -34,7 +34,7 @@ export const DataServices = [
   {
     id: 4,
     urlImage: "/video/InstalaciondePisos.mp4",
-    name: "Instalacion De Pisos Spc y pvc De click y Laminados",
+    name: "Instalación de pisos SPC y PVC de click y laminados",
     icon: <MdOutlineConstruction className="icon-clean" />,
     description:
       "¡Pisos elegantes y duraderos! Nuestra amplia variedad de pisos SPC y laminados te permitirá encontrar el estilo perfecto para tu hogar. Contamos con instaladores expertos para un acabado impecable.",
@@ -43,7 +43,7 @@ export const DataServices = [
   {
     id: 5,
     urlImage: "/video/InstalacionPersianas.mp4",
-    name: "Instalacion y Rstauracion De Persianas Estilo Para tus Ventanas",
+    name: "Instalación y retauración de persianas estilo para tus ventanas",
     icon: <BsWrenchAdjustableCircle className="icon-clean" />,
     description:
       "Dale un toque de elegancia y funcionalidad a tus ventanas con nuestras persianas de diseño. Ofrecemos una amplia gama de estilos y materiales para adaptarse a cualquier ambiente.",
@@ -52,7 +52,7 @@ export const DataServices = [
   {
     id: 6,
     urlImage: "/video/papelColgadura.mp4",
-    name: "Paredes con Estilo Instalación de Papel Tapiz",
+    name: "Paredes con estilo instalación de papel tapiz",
     icon: <SiAffinitydesigner className="icon-clean" />,
     description:
       "Transforma tus paredes en obras de arte con nuestros exclusivos papeles tapiz. Una amplia variedad de diseños y texturas para crear ambientes únicos y personalizados.",
@@ -60,8 +60,8 @@ export const DataServices = [
   },
   {
     id: 7,
-    urlImage: "/video/InstalaciondePisos.mp4",
-    name: "Tu Hogar Fresco con Películas de Protección Solar",
+    urlImage: "/video/peliculaSolar.mp4",
+    name: "Tu hogar fresco con películas de protección solar",
     icon: <AiOutlineSecurityScan className="icon-clean" />,
     description:
       " Disfruta de un hogar más fresco y protegido del sol con nuestras películas de protección solar. Bloquea los rayos UV, reduce el calor y mejora la privacidad.",
@@ -70,7 +70,7 @@ export const DataServices = [
   {
     id: 8,
     urlImage: "/video/InstalacionToldosYPergolas.mp4",
-    name: "Instalacion y restauracion de Toldos y Pérgolas",
+    name: "Instalación y restauración de toldos y Pérgolas",
     icon: <BsWrenchAdjustableCircle className="icon-clean" />,
     description:
       "Crea espacios exteriores acogedores y funcionales con nuestros toldos y pérgolas. Protección solar, lluvia y viento para disfrutar al máximo de tus espacios al aire libre.",
@@ -79,10 +79,10 @@ export const DataServices = [
   {
     id: 9,
     urlImage: "/video/InstalacionDePanelWall.mp4",
-    name: "Instalacion de panelWall",
+    name: "Instalación de panelWall",
     icon: <MdOutlineConstruction className="icon-clean" />,
     description:
-      "Con Nuestro panel wall tendras Una solución práctica y estética para cubrir paredes, crear divisiones o agregar detalles arquitectónicos.",
+      "Con Nuestro panel wall tendras una solución práctica y estética para cubrir paredes, crear divisiones o agregar detalles arquitectónicos.",
     ref: "9",
   },
   {
@@ -94,14 +94,13 @@ export const DataServices = [
       "Transforma tu jardín en un oasis verde con nuestra grama sintética. Ofrecemos una amplia variedad de modelos y texturas, adaptados a cualquier espacio y necesidad. Nuestros expertos se encargan de la instalación completa, desde la preparación del terreno hasta los acabados finales. Disfruta de un césped siempre perfecto, sin necesidad de riego ni mantenimiento.",
     ref: "10",
   },
-  /*falta video madera maciza*/
   {
     id: 11,
-    urlImage:"/video/InstalaciondePisos.mp4",
-    name: "Restauracion E Instalacion de Pisos En Madera",
+    urlImage:"/video/pisosEnMadera.mp4",
+    name: "Inatalación y restauración de pisos en madera",
     icon: <MdOutlineConstruction className="icon-clean" />,
     description:
-      "Transforma tu jardín en un oasis verde con nuestra grama sintética. Ofrecemos una amplia variedad de modelos y texturas, adaptados a cualquier espacio y necesidad. Nuestros expertos se encargan de la instalación completa, desde la preparación del terreno hasta los acabados finales. Disfruta de un césped siempre perfecto, sin necesidad de riego ni mantenimiento.",
+      "Renueva tus espacios con la elegancia y calidez de nuestros pisos de madera maciza. Contamos con una amplia variedad de estilos y acabados, ideales para cualquier ambiente y necesidad. Nuestro equipo de expertos se encarga de la instalación completa, desde la preparación de la superficie hasta los detalles finales. Disfruta de un suelo duradero, sofisticado y natural, que añade valor y belleza a tu hogar sin perder su esencia con el tiempo.",
     ref: "11",
   },
 ];
